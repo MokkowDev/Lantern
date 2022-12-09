045cd36963af0d7f648dc2a1b22ea408528ff746
TheMosKau <rdeefpctest@gmail.com>
2022-12-09 19:50:33 +0700

    Some fix

--- src/main/java/dev/tr7zw/waveycapes/WaveyCapesBase.java
+++ src/main/java/dev/tr7zw/waveycapes/WaveyCapesBase.java
@@ -20,73 +20,17 @@ public abstract class WaveyCapesBase {
 
     public static WaveyCapesBase INSTANCE;
     public static final Logger LOGGER = LogManager.getLogger("WaveyCapes");
-    public static Config config;
     private final File settingsFile = new File("config", "waveycapes.json");
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private boolean optifinePresent = false;
     
     public void init() {
         INSTANCE = this;
-        if (settingsFile.exists()) {
-            try {
-                config = gson.fromJson(new String(Files.readAllBytes(settingsFile.toPath()), StandardCharsets.UTF_8),
-                        Config.class);
-            } catch (Exception ex) {
-                System.out.println("Error while loading config! Creating a new one!");
-                ex.printStackTrace();
-            }
-        }
-        if (config == null) {
-            config = new Config();
-            writeConfig();
-        } else {
-            if(ConfigUpgrader.upgradeConfig(config)) {
-                writeConfig();
-            }
-        }
         initSupportHooks();
     }
     
     public void writeConfig() {
-        if (settingsFile.exists())
-            settingsFile.delete();
-        try {
-            Files.write(settingsFile.toPath(), gson.toJson(config).getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-    }
-    
-    public GuiScreen createConfigScreen(GuiScreen parent) {
-        return new ConfigScreen(parent);
-    }
-    
-    public static class ConfigScreen extends CustomConfigScreen {
-
-        public ConfigScreen(GuiScreen lastScreen) {
-            super(lastScreen, "text.wc.title");
-        }
-        
-        @Override
-        public void initialize() {
-            List<GuiButton> options = new ArrayList<>();
-            options.add(getEnumOption("text.wc.setting.capestyle", CapeStyle.class, () -> config.capeStyle, (v) -> config.capeStyle = v));
-            options.add(getEnumOption("text.wc.setting.windmode", WindMode.class, () -> config.windMode, (v) -> config.windMode = v));
-            options.add(getEnumOption("text.wc.setting.capemovement", CapeMovement.class, () -> config.capeMovement, (v) -> config.capeMovement = v));
-            //options.add(getIntOption("text.wc.setting.capeparts", 16, 64, () -> config.capeParts, (v) -> config.capeParts = v));
-            options.add(getIntOption("text.wc.setting.gravity", 5, 32, () -> config.gravity, (v) -> config.gravity = v));
-            options.add(getIntOption("text.wc.setting.heightMultiplier", 4, 16, () -> config.heightMultiplier, (v) -> config.heightMultiplier = v));
-            //options.add(getIntOption("text.wc.setting.maxBend", 1, 20, () -> config.maxBend, (v) -> config.maxBend = v));
-
-            addOptionsList(options);
-
-        }
-
-        @Override
-        public void save() {
-            INSTANCE.writeConfig();
-        }
-        
+        // moved to wavey cape module
     }
     
     public abstract void initSupportHooks();
